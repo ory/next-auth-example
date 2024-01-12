40d08f60b74492a131c8cf1414461e3f7b77d0fc
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-01-12 14:04:39 +0100

    feat: add OIDC logout

--- components/auth-components.tsx
+++ components/auth-components.tsx
@@ -1,5 +1,6 @@
-import { signIn, signOut } from "auth"
+import { signIn, signOut, auth } from "auth"
 import { Button } from "./ui/button"
+import { redirect } from "next/navigation"
 
 export function SignIn({
   provider,
@@ -17,12 +18,23 @@ export function SignIn({
   )
 }
 
-export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
+export async function SignOut(
+  props: React.ComponentPropsWithRef<typeof Button>,
+) {
+  const session = await auth()
+
+  const redirectTo = "http://localhost:3000/" // This has to be in the list of allowed post_logout_redirect_uris in the OAuth2 Client.
+  const oidcLogout =
+    process.env.ORY_SDK_URL +
+    `/oauth2/sessions/logout?id_token_hint=${session?.idToken}&post_logout_redirect_uri=${redirectTo}`
   return (
     <form
       action={async () => {
         "use server"
-        await signOut()
+        await signOut({
+          redirect: false,
+        })
+        redirect(oidcLogout)
       }}
       className="w-full"
     >
